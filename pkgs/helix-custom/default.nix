cffb93614b053dfcf63853819ef7cae3c95a47f1
spaceavenue <spaceavenue@proton.me>
2026-09-01 16:47:19 +0530

    fix helix

--- pkgs/helix-custom/default.nix
+++ pkgs/helix-custom/default.nix
@@ -1,4 +1,5 @@
-{ lib, rustPlatform, runCommand, installShellFiles }:
+{ lib, rustPlatform, runCommand, installShellFiles, makeBinaryWrapper
+, tree-sitter-grammars, removeReferencesTo }:
 
 let
   src = builtins.fetchGit {
@@ -6,9 +7,27 @@ let
     ref = "master";
   };
 
-  defaultRuntimeDir = runCommand "helix-custom-default-runtime" { } ''
-    cp -r --no-preserve=mode ${src}/runtime $out
-    rm -rf $out/grammars $out/queries
+  languages = builtins.fromTOML (builtins.readFile "${src}/languages.toml");
+  grammarNames = map (g: g.name) languages.grammar;
+
+  # Intersection of the grammars helix declares in its languages.toml and the ones nixpkgs happens
+  # to package under the matching tree-sitter-<name> name. A handful newer/renamed grammars that
+  # aren't in nixpkgs yet are skipped.
+  availableGrammars = lib.filterAttrs
+    (drvName: _: builtins.elem (lib.removePrefix "tree-sitter-" drvName) grammarNames)
+    tree-sitter-grammars;
+
+  grammarsFarm = runCommand "helix-custom-grammars" { } (
+    lib.concatMapAttrsStringSep "\n" (_: grammar: ''
+      install -D ${grammar}/parser $out/${grammar.language}.so
+      ${lib.getExe removeReferencesTo} -t ${grammar} $out/${grammar.language}.so
+    '') availableGrammars
+  );
+
+  runtimeDir = runCommand "helix-custom-runtime" { } ''
+    mkdir -p $out
+    ln -s ${grammarsFarm} $out/grammars
+    cp -r --no-preserve=mode ${src}/runtime/queries $out
   '';
 in
 rustPlatform.buildRustPackage {
@@ -19,22 +38,24 @@ rustPlatform.buildRustPackage {
 
   cargoLock.lockFile = "${src}/Cargo.lock";
 
-  nativeBuildInputs = [ installShellFiles ];
+  nativeBuildInputs = [ installShellFiles makeBinaryWrapper ];
 
   env = {
     HELIX_DISABLE_AUTO_GRAMMAR_BUILD = "1";
-    HELIX_DEFAULT_RUNTIME = defaultRuntimeDir;
     RUSTFLAGS = "-C target-cpu=native";
   };
 
   postInstall = ''
     installShellCompletion contrib/completion/hx.{bash,fish,zsh}
+    wrapProgram $out/bin/hx --set HELIX_RUNTIME "${runtimeDir}"
   '';
 
   doCheck = false;
 
+  passthru.runtime = runtimeDir;
+
   meta = {
-    description = "Post-modern modal text editor (git version)";
+    description = "Post-modern modal text editor (built from master branch)";
     homepage = "https://helix-editor.com";
     license = lib.licenses.mpl20;
     mainProgram = "hx";
